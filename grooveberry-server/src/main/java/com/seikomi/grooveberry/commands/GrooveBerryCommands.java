751f23bc3d803a973db25aaf8dd9c0e3480fba5f
Sw4T <pierre.gambier@yahoo.fr>
2018-01-14 02:36:39 +0100

    Added a serialized method for the reading queue

--- grooveberry-server/src/main/java/com/seikomi/grooveberry/commands/GrooveBerryCommands.java
+++ grooveberry-server/src/main/java/com/seikomi/grooveberry/commands/GrooveBerryCommands.java
@@ -1,5 +1,28 @@
 package com.seikomi.grooveberry.commands;
 
-public enum GrooveBerryCommands {
-
+public enum GrooveBerryCommands { 
+	
+	DOWNLOAD("#DOWNLOAD"),
+	UPLOAD("#UPLOAD"),
+	NEXT("#NEXT"), 
+	PREV("#PREV"),
+	PLAY("#PLAY"),
+	PAUSE("#PAUSE"),
+	STOP("#STOP"),
+	VOLUME_UP("#VOLUP"),
+	VOLUME_DOWN("#VOLDOWN"),
+	CURRENT_SONG("#SONG"),
+	GET_READING_QUEUE("#LIST"),
+	EXIT("#EXIT");
+	
+	private String command;
+	
+	private GrooveBerryCommands(String command) {
+		this.command = command;
+	}
+	
+	public String getCommand() {
+		return this.command;
+	}
+	
 }
